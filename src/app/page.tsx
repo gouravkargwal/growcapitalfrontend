85c8cec6ee405a873309b9061b56c8cae9adeddf
Gourav Kargwal <gouravkargwalverma@gmail.com>
2024-11-11 20:59:18 +0530

    fix: Fixed landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,26 @@ import Footer from "../Components/Header/Footer";
 import Hero from "@/Components/Home/Hero";
 import PricingSection from "@/Components/Home/Pricing";
 import PublicNavbar from "../Components/Header/PublicNavbar";
-import React from "react";
+import React, { useEffect } from "react";
 import Reviews from "@/Components/Home/Reviews";
 import Trial from "@/Components/Home/Trial";
+import { usePathname, useRouter } from "next/navigation";
+import { useAuth } from "@/lib/firebase";
 
 const Home: React.FC = () => {
+  const router = useRouter();
+  const pathname = usePathname(); // Get the current route in Next.js App Router
+  const { user, loading } = useAuth(); // Use your hook to get auth state
+
+  useEffect(() => {
+    if (loading) return; // Wait until loading is complete
+
+    // Redirect to dashboard only if user is authenticated and on the public landing page
+    if (user && pathname === "/") {
+      router.push("/dashboard");
+    }
+  }, [user, loading, router, pathname]);
+
   return (
     <div className="bg-gradient-to-br from-blue-50 via-blue-50 to-white">
       <PublicNavbar />
