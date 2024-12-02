c46a8dca1970fcc47f8da360222bdb3b7196fcf2
Hirdesh <fello@fellos-MacBook-Pro.local>
2024-12-02 14:10:17 +0530

    fix: for video

--- src/app/(protected)/subscriptions/page.tsx
+++ src/app/(protected)/subscriptions/page.tsx
@@ -38,7 +38,7 @@ const Subscriptions = () => {
 
         <CurrentPlan />
         <UpgradePlan />
-        <ComparePlans />
+        {/* <ComparePlans /> */}
         <PlanHistory />
       </div>
     </>
